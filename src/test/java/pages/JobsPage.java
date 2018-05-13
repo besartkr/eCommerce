cc2337369450a3ff59cfb8dac2f3cf207c8843e1
Besart <besartk@hotmail.co.uk>
2018-05-13 20:26:30 +0100

    login

--- src/test/java/pages/JobsPage.java
+++ src/test/java/pages/JobsPage.java
@@ -1,8 +1,10 @@
 package pages;
 
 import Base.BaseUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -14,10 +16,16 @@ public class JobsPage extends BaseUtil {
         PageFactory.initElements ( webDriver, this );
     }
 
+/*
+Selectors for the Jobs Admin tab
+ */
+
+    @FindBy(css = "[id='menu_admin_viewAdminModule']")
+    public WebElement adminTab;
 
 
-@FindBy (id = "menu_admin_Job")
-public WebElement JobMenuSelector;
+    @FindBy (id = "menu_admin_Job")
+    public WebElement JobMenuSelector;
 
     @FindBy(id = "menu_admin_viewJobTitleList")
     public WebElement JobTItleListSelector;
@@ -33,5 +41,17 @@ public WebElement JobMenuSelector;
 
     @FindBy (id = "menu_admin_workShift")
     public  WebElement workShiftSelector;
-}
 
+    public void adminTab () {
+
+        Actions action = new Actions (webDriver);
+        WebElement we = webDriver.findElement(By.cssSelector ("[id='menu_admin_viewAdminModule']"));
+        WebElement we1 = webDriver.findElement(By.id ("menu_admin_Job"));
+        action.moveToElement (we).build ().perform ();
+        action.moveToElement (we1).build ().perform ();
+
+        JobTItleListSelector.click ();
+
+
+    }
+}
