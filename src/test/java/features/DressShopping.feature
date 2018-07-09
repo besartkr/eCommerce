b76b8d91b3e40290d8b7ef8cd9bf0c54fb9b7cf8
Besart.Kryeziu <besartk@hotmail.co.uk>
2018-07-09 10:08:48 +0100

    DressShopping

---
+++ src/test/java/features/DressShopping.feature
@@ -0,0 +1,32 @@
+Feature: ContactUs feature
+
+  Background:
+    Given I navigate to the home page
+
+  @ready
+  Scenario: Navigate to Contact us page and send a message
+    Given I navigate to the contact us page
+    When I enter the  email address, orderRef & the Message
+    Then I will get a confirmation message
+
+
+  @ready
+  Scenario: Login to the site
+    Given I navigate to the Login Page
+    When I login as a register user
+    Then I will be taken to My Account page
+
+
+  @ready
+  Scenario: Ensure that I have items in my WishList
+    Given I navigate to the WishList Page
+    When I add items to my WishList
+    Then  I will see list of WishList items on my WishList table
+
+
+  @test
+  Scenario: Ensure I can increase the quanity of the product I have in my WishList table
+    Given I navigate to the WishList Page
+    When I increase the product quantity To 10
+    Then the new quantity will be saved and displayed
+
---
+++ src/test/java/steps/DressShoppingSteps.java
@@ -0,0 +1,74 @@
+
+package steps;
+
+import Base.BaseUtil;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import pages.LoginPage;
+import pages.PracticeContactUSPage;
+import pages.PracticeLoginPage;
+
+
+public class PracticeContactUs extends BaseUtil {
+
+    private BaseUtil base;
+
+    public PracticeContactUs(BaseUtil webDriver) {
+        this.base = webDriver;
+    }
+
+
+    @Given("^I navigate to the contact us page$")
+    public void iNavigateToTheContactUsPage() throws Throwable {
+        PracticeContactUSPage page = new PracticeContactUSPage(base.webDriver);
+        page.contactUs();
+
+    }
+
+
+    @When("^I enter the  email address, orderRef & the Message$")
+    public void iEnterTheEmailAddressOrderRefTheMessage() throws Throwable {
+        PracticeContactUSPage page = new PracticeContactUSPage(base.webDriver);
+        page.messageDetails();
+
+    }
+
+
+    @Then("^I will get a confirmation message$")
+    public void iWillGetAConfirmationMessage() throws Throwable {
+        PracticeContactUSPage page = new PracticeContactUSPage(base.webDriver);
+        page.sendMessage();
+    }
+
+    /*     Login page      */
+    @Given("^I navigate to the Login Page$")
+    public void iNavigateToTheLoginPage() throws Throwable {
+        PracticeLoginPage page = new PracticeLoginPage(base.webDriver);
+        page.loginLink();
+
+
+    }
+
+    @When("^I login as a register user$")
+    public void iLoginAsARegisterUser() throws Throwable {
+        PracticeLoginPage page = new PracticeLoginPage(base.webDriver);
+        page.loginEmail();
+        page.loginPassword();
+
+
+    }
+
+    @Then("^I will be taken to My Account page$")
+    public void iWillBeTakenToMyAccountPage() throws Throwable {
+        PracticeLoginPage page = new PracticeLoginPage(base.webDriver);
+        page.loginSubmit();
+    }
+
+}
